8049f2c23de0743848b2243dfea08fb269b0a5e8
Anonymous <>
2024-05-23 09:15:09 +0200

    utils docs

--- shared/redux/utils.ts
+++ shared/redux/utils.ts
@@ -7,6 +7,12 @@ import {
   WatchListState,
 } from "../interfaces/trending";
 
+/**
+ * Updates the state by pushing a list of media items based on the action type.
+ *
+ * @param {TrendingState} state - The current trending state object.
+ * @param {PayloadAction<Trending>} action - The action object containing trending data.
+ */
 export const pushMediaList = (
   state: TrendingState,
   action: PayloadAction<Trending>
@@ -24,10 +30,18 @@ export const pushMediaList = (
   state[filter].total_pages = action.payload.total_pages;
   state[filter].total_results = action.payload.total_results;
   state[filter].results.push(
-    action.payload.results.filter((el) => el.media_type !== "person")
+    action.payload.results.filter((el) => el.media_type !== "person") // Don't push trending people
   );
 };
 
+/**
+ * Finds the position of a media item in the watchlist state based on mediaId and filter.
+ *
+ * @param {WatchListState} state - The current watchlist state object.
+ * @param {number} mediaId - The ID of the media item to find.
+ * @param {Filter} filter - The filter to be applied for search.
+ * @returns {MediaPosition} The position information of the media item.
+ */
 export function findMediaPosition(
   state: WatchListState,
   mediaId: number,
