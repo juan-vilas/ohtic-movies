02527cfaef257d3ad2f1abb41c51e643f4005189
Anonymous <>
2024-05-23 09:11:40 +0200

    watchlist docs

--- shared/redux/watchlist.ts
+++ shared/redux/watchlist.ts
@@ -4,13 +4,19 @@ import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { MediaData, WatchListState } from "../interfaces/trending";
 import { findMediaPosition } from "./utils";
 
+// Initial state for the watchlist, containing empty results for all, movie, and tv filters.
 const initialState: WatchListState = {
   all: { results: [] },
   movie: { results: [] },
   tv: { results: [] },
 };
 
-export const getStorage = createAsyncThunk<WatchListState>(
+/**
+ * Asynchronous thunk function to retrieve the watchlist state from AsyncStorage.
+ *
+ * @returns {Promise<WatchListState>} A promise that resolves to the retrieved watchlist state.
+ */
+const getStorage = createAsyncThunk<WatchListState>(
   "watchlist/getStorage",
   async () => {
     const value = await AsyncStorage.getItem("watchlist");
@@ -23,10 +29,18 @@ export const getStorage = createAsyncThunk<WatchListState>(
   }
 );
 
-export const watchListSlice = createSlice({
+const watchListSlice = createSlice({
   name: "watchList",
   initialState,
   reducers: {
+    /**
+     * Removes a media item from the state based on the provided mediaId and filter.
+     *
+     * @param {WatchListState} state - The current state object.
+     * @param {PayloadAction<{ mediaId: number; filter: Filter }>} action - The action object containing mediaId and filter.
+     * @param {number} action.mediaId - The ID of the media item to be removed.
+     * @param {Filter} action.filter - The filter to be applied for removal.
+     */
     removeMedia: (
       state,
       action: PayloadAction<{ mediaId: number; filter: Filter }>
@@ -41,6 +55,16 @@ export const watchListSlice = createSlice({
       }
       AsyncStorage.setItem("watchlist", JSON.stringify(state));
     },
+    /**
+     * Adds a new media item to the state based on the provided mediaData and filter.
+     * If the watchlist is empty, it creates a new page and adds the media item.
+     * If the watchlist is not empty, it adds the media item to the first available page.
+     *
+     * @param {WatchListState} state - The current state object.
+     * @param {PayloadAction<{ mediaData: MediaData; filter: Filter }>} action - The action object containing mediaData and filter.
+     * @param {MediaData} action.mediaData - The data of the media item to be added.
+     * @param {Filter} action.filter - The filter to be applied for addition.
+     */
     addMedia: (
       state,
       action: PayloadAction<{ mediaData: MediaData; filter: Filter }>
@@ -67,7 +91,9 @@ export const watchListSlice = createSlice({
       }
     },
   },
+
   extraReducers: (builder) => {
+    // Gets watchlist fron AsyncStorage if it exists
     builder.addCase(getStorage.fulfilled, (state, action) => {
       state = action.payload;
       return state;
